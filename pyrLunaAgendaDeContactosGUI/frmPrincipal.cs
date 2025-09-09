038dc297a75560fe4915286d9c8ba80669faea44
TomasLuna <Conradoluna20@gmail.com>
2025-09-09 13:53:13 -0300

    mas cambios

--- pyrLunaAgendaDeContactosGUI/frmPrincipal.cs
+++ pyrLunaAgendaDeContactosGUI/frmPrincipal.cs
@@ -12,6 +12,14 @@ namespace pyrLunaAgendaDeContactosGUI
 {
     public partial class frmPrincipal : Form
     {
+        //variables
+        string vContacto = "";
+        string vNumero = "";
+        //vectores
+        string[] vecContacto = new string[5];
+        string[] vecNumero = new string[5];
+        //indice
+        int indice = 0;
         public frmPrincipal()
         {
             InitializeComponent();
@@ -19,14 +27,17 @@ namespace pyrLunaAgendaDeContactosGUI
 
         private void btnRegistrar_Click(object sender, EventArgs e)
         {
-            String vContacto = txtContacto.Text;
-            String vNumero = mskNumero.Text;
+            vContacto = txtContacto.Text;
+            vNumero = mskNumero.Text;
+            vecContacto[indice] = vContacto;
+            vecNumero[indice] = vNumero;
             lstRegistro.Items.Add(vContacto + " - " + vNumero);
             lblCantidad.Text = "Cantidad de contactos: " + lstRegistro.Items.Count.ToString();
             lblFechaYHora.Text = "Fecha y hora: " + DateTime.Now.ToString();
             txtContacto.Text = "";
             mskNumero.Text = "";
             txtContacto.Focus();
+            indice++;
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
@@ -34,6 +45,30 @@ namespace pyrLunaAgendaDeContactosGUI
             txtContacto.Text = "";
             mskNumero.Text = "";
             txtContacto.Focus();
+        }
+
+        private void txtContacto_TextChanged(object sender, EventArgs e)
+        {
+            if (txtContacto.Text != "" && mskNumero.Text != "")
+            {
+                mskNumero.Enabled = true;
+            }
+            else
+            {
+                mskNumero.Enabled = false;
+            }
+        }
+
+        private void mskNumero_TextChanged(object sender, EventArgs e)
+        {
+            if (txtContacto.Text != "" && mskNumero.Text != "")
+            {
+                btnRegistrar.Enabled = true;
+            }
+            else
+            {
+                btnRegistrar.Enabled = false;
+            }
         }
     }
 }
